3dced32ac44081c4252b31b33f5fa55d6c8993a9
Diego Alejandro Vanegas Zúñiga <dieguitovan98@hotmail.com>
2019-03-28 10:41:27 -0600

    test commit

---
+++ Greenit/Migrations/20190328163958_testmi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Greenit.Migrations
+{
+    public partial class testmi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
